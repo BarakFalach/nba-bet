2bb9efb1201dbd50119ffe1248e5af9e88859f1a
Guy Meyer <guymeyer1995@gmail.com>
2025-03-22 23:31:30 +0200

    add bet tracking via new games added

---
+++ worker/src/db_connect/bets_update.py
@@ -0,0 +1,37 @@
+import json 
+def isBetExist(supabase_client, game_id, user_id, all_bets):
+
+    for e in all_bets['data']:
+        if e['eventId'] == game_id and e['userId'] == user_id:
+            return True
+    return False
+
+def updateBets(supabase_client, game_data):
+
+    response = supabase_client.table('users').select('id').execute()
+    user_list=json.loads(response.json())
+    uids = user_list["data"]
+
+    response = supabase_client.table('bets').select('*').execute()
+    all_bets=json.loads(response.json())
+
+    # if bet not exist then create on for each user in UserList
+    for user in uids:
+        gid = game_data["id"]
+        uid = user['id']
+        
+        if not isBetExist(supabase_client, gid, uid, all_bets):
+            print("adding new")
+            bet_data = {
+                "eventId": gid,
+                "userId": int(uid),
+                "eventType": game_data["eventType"],
+                "closeTime": game_data["startTime"]
+            }
+            response = supabase_client.table('bets').upsert(bet_data).execute()
+    
+
+    return {
+        'statusCode': 200,
+        'body': ":)"
+    }
--- worker/src/db_connect/lambda_function.py
+++ worker/src/db_connect/lambda_function.py
@@ -1,7 +1,7 @@
 import os
 import supabase
-from .scoreboard import getTodaysGames
-from datetime import datetime
+from .nba_api_interface import getTodaysGames
+from .bets_update import updateBets
 
 
 supabase_url = os.environ.get('SUPABASE_URL')
@@ -9,19 +9,16 @@ supabase_key = os.environ.get('SUPABASE_ANON_KEY')
 supabase_client = supabase.create_client(supabase_url, supabase_key)
 
 def lambda_handler(event, context):
-    response = supabase_client.table('events').select('*').execute()
+    # supabase_client.table('events').select('*').execute()
 
-    # Example usage
     todays_games = getTodaysGames()
     for game_data in todays_games:
-        print(game_data)
-        response = supabase_client.table('events').upsert(game_data).execute()
-        if response.status_code == 201:
-            print("Data upserted successfully!")
-        else:
-            print(f"Failed to upsert data: {response.json()}")
+        
+        # Insert or update event data
+        supabase_client.table('events').upsert(game_data).execute()
 
-        print("\n\n")
+        # Insert or update bets data per user
+        updateBets(supabase_client, game_data)
 
 
     return {
