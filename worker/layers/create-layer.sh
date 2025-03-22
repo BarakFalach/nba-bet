45a5a9227f1ace2b1bc682484dc8dc95abe48b95
Guy Meyer <guymeyer1995@gmail.com>
2025-03-22 21:53:33 +0200

    add layers for nba_api

--- worker/layers/create-layer.sh
+++ worker/layers/create-layer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PACKAGE_DIR=python/lib/python3.10/site-packages/
+LIBS="pydantic-core supabase nba_api requests numpy"
 
 # make path if it doesnt exist
 mkdir -p ${PACKAGE_DIR}
@@ -8,7 +9,7 @@ mkdir -p ${PACKAGE_DIR}
 rm -rf ${PACKAGE_DIR}/*
 
 cd ${PACKAGE_DIR}
-pip3 install pydantic-core supabase --platform manylinux2014_x86_64 -t . --python-version 3.10 --only-binary=:all:
+pip3 install ${LIBS} --platform manylinux2014_x86_64 -t . --python-version 3.10 --only-binary=:all:
 cd -
 
 zip -r supabase-linux2014-x86_64-py3_10.zip python
