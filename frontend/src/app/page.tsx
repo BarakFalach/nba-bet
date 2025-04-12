2aec10794e1f74e1ac33f24cc46a3042528f6e0e
BarakFalach <barakf@wix.com>
2025-04-12 12:46:58 +0300

    ux align

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -13,7 +13,7 @@ function MainPage() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen px-4 dark:bg-black">
+    <div className="flex flex-col items-center justify-top min-h-screen px-4 dark:bg-black">
       <div className="w-full max-w-2xl text-center space-y-6">
         <h1 className="text-4xl md:text-5xl font-bold text-gray-900 dark:text-gray-100">
           Welcome {user?.user_metadata?.displayName || 'to Bet Tracker'}
